402a5f3f3f3e2b1b063a387ae6e0014daddc7525
Gilad-MD <dudugilad@gmail.com>
2025-10-03 20:35:18 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import RomeWeatherWidget from "./RomeWeatherWidget"
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-6">
+      <RomeWeatherWidget />
+    </div>
+  )
+}
